3e4ba3987f6ab2ca1eff69f36be5f57944447cca
pedroiley <pedro.iglesias.leyva@gmail.com>
2020-11-01 10:34:12 +0200

    Add description to sales manager class

--- src/main/java/Models/Product.java
+++ src/main/java/Models/Product.java
@@ -1,4 +1,61 @@
 package Models;
 
 public class Product {
+
+    private int id;
+    private String type;
+    private int price;
+    private String name;
+    private int amount;
+
+    public Product(int id, String type, int price, String name, int amount) {
+        this.id = id;
+        this.type = type;
+        this.price = price;
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public Product() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
--- src/main/java/Models/SalesManager.java
+++ src/main/java/Models/SalesManager.java
@@ -1,4 +1,52 @@
 package Models;
 
 public class SalesManager {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private boolean isAvailable;
+
+
+    public SalesManager(int id, String firstName, String lastName, boolean isAvailable) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.isAvailable = isAvailable;
+    }
+
+    public SalesManager() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
 }
