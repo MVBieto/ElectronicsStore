1eebec3b1cf982708b4e09fc23319eb052efc9ff
pedroiley <pedro.iglesias.leyva@gmail.com>
2020-11-02 13:32:32 +0200

    Update SalesManager Class

--- src/main/java/Models/SalesManager.java
+++ src/main/java/Models/SalesManager.java
@@ -1,15 +1,41 @@
 package Models;
 
+import java.util.Date;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+
+@Entity
+@Table(schema = "ElectronicStore", name = "SalesManager")
+
+
+
 public class SalesManager {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+    @Column(name = "firstName")
     private String firstName;
+    @Column(name = "lastName")
     private String lastName;
+    @Column(name = "isAvailable")
     private boolean isAvailable;
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private Date createdAt;
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "last_updated")
+    private Date lastUpdated;
 
 
-    public SalesManager(int id, String firstName, String lastName, boolean isAvailable) {
-        this.id = id;
+
+
+    public SalesManager(String firstName, String lastName, boolean isAvailable) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.isAvailable = isAvailable;
@@ -48,5 +74,21 @@ public class SalesManager {
 
     public void setAvailable(boolean available) {
         isAvailable = available;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
     }
 }
