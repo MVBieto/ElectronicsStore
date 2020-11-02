97d141daefdb9b352c0cde1e9c34853f0a5914f0
pedroiley <pedro.iglesias.leyva@gmail.com>
2020-11-02 12:38:12 +0200

    SalesManagerDao commit

---
+++ src/main/java/Dao/SalesManagerDao.java
@@ -0,0 +1,72 @@
+package Dao;
+import Models.SalesManager;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import Util.Hibernate;
+public class SalesManagerDao {
+    public SalesManagerDao() {
+    }
+    public void createSalesManager(SalesManager salesManager) {
+        Session session = Hibernate.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.save(salesManager);
+            transaction.commit();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        session.close();
+    }
+    public SalesManager getSalesManager(Long salesManagerId) {
+        Session session = Hibernate.getSessionFactory().openSession();
+        try {
+            SalesManager salesManager = session.find(SalesManager.class, salesManagerId);
+            session.close();
+            return salesManager;
+        } catch (Exception ex) {
+            session.close();
+            System.out.println("Unable to find the SalesManager with id: "+ salesManagerId);
+            ex.printStackTrace();
+            return null;
+        }
+    }
+    public List<SalesManager> getSalesManager(){
+        Session session = Hibernate.getSessionFactory().openSession();
+        return session.createQuery("from SalesManager", SalesManager.class).list();
+    }
+    public void updateSalesManager(SalesManager savedSalesManager) {
+        Session session = Hibernate.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.update(savedSalesManager);
+            transaction.commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        session.close();
+    }
+    public void deleteSalesManager(SalesManager savedSalesManager) {
+        Session session = Hibernate.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.delete(savedSalesManager);
+            transaction.commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        session.close();
+    }
+}
