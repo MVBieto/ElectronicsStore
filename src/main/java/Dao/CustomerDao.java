71351b69786accd9d89c92fa3dd6a80353e88374
pedroiley <pedro.iglesias.leyva@gmail.com>
2020-11-02 12:27:13 +0200

    Update product dao (#7)
    * ProductDao commit
    
    * ProductDao commit
    
    * ProductDao commit 2.0
    
    * Add CustomerDao
    
    * Fix typos
    
    Co-authored-by: Miquel Vidal <miquelvidalbieto@gmail.com>

---
+++ src/main/java/Dao/CustomerDao.java
@@ -0,0 +1,80 @@
+package Dao;
+
+import Models.Customer;
+import Util.Hibernate;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import java.util.List;
+
+public class CustomerDao {
+
+    public CustomerDao() {
+    }
+
+    public void createCustomer(Customer customer) {
+        Session session = Hibernate.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.save(customer);
+            transaction.commit();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        session.close();
+    }
+
+    public Customer getCustomer(Long customerId) {
+        Session session = Hibernate.getSessionFactory().openSession();
+        try {
+            Customer customer = session.find(Customer.class, customerId);
+            session.close();
+            return customer;
+        } catch (Exception ex) {
+            session.close();
+            System.out.println("Unable to find the customer with id: " + customerId);
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<Customer> getCustomers() {
+        Session session = Hibernate.getSessionFactory().openSession();
+        return session.createQuery("from Customer", Customer.class).list();
+    }
+
+    public void updateCustomer(Customer savedCustomer) {
+        Session session = Hibernate.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.update(savedCustomer);
+            transaction.commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        session.close();
+    }
+
+    public void deleteCustomer(Customer savedCustomer) {
+        Session session = Hibernate.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.delete(savedCustomer);
+            transaction.commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        session.close();
+    }
+}
---
+++ src/main/java/Dao/ProductDao.java
@@ -0,0 +1,72 @@
+package Dao;
+import Models.Product;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import Util.Hibernate;
+public class ProductDao {
+    public ProductDao() {
+    }
+    public void createProduct(Product product) {
+        Session session = Hibernate.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.save(product);
+            transaction.commit();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        session.close();
+    }
+    public Product getProduct(Long productId) {
+        Session session = Hibernate.getSessionFactory().openSession();
+        try {
+            Product product = session.find(Product.class, productId);
+            session.close();
+            return product;
+        } catch (Exception ex) {
+            session.close();
+            System.out.println("Unable to find the product with id: "+ productId);
+            ex.printStackTrace();
+            return null;
+        }
+    }
+    public List<Product> getProducts(){
+        Session session = Hibernate.getSessionFactory().openSession();
+        return session.createQuery("from Product", Product.class).list();
+    }
+    public void updateProduct(Product savedProduct) {
+        Session session = Hibernate.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.update(savedProduct);
+            transaction.commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        session.close();
+    }
+    public void deleteProduct(Product savedProduct) {
+        Session session = Hibernate.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.delete(savedProduct);
+            transaction.commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+        session.close();
+    }
+}
