ce0a1821f6f4db54c36485953df1da5f9cf97eaa
Miquel Vidal <miquelvidalbieto@gmail.com>
2020-11-03 20:31:10 +0200

    Fixing Database. Contains issues.

--- src/main/java/Util/Hibernate.java
+++ src/main/java/Util/Hibernate.java
@@ -23,25 +23,29 @@ public class Hibernate {
             Configuration configuration = new Configuration();
             Properties properties = new Properties();
             properties.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
-            properties.put(Environment.URL, "jdbc:mysql://localhost:3306/users");
+            properties.put(Environment.URL, "jdbc:mysql://localhost:3306/ElectronicStore?createDatabaseIfNotExist=true");
             properties.put(Environment.USER, applicationProperties.getString("dbUsername"));
             properties.put(Environment.PASS, applicationProperties.getString("dbPassword"));
             properties.put(Environment.SHOW_SQL, "true");
             properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
-//            properties.put(Environment.HBM2DDL_AUTO, "create-drop");
+            properties.put(Environment.HBM2DDL_AUTO, "create-drop");
             properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
             configuration.setProperties(properties);
+
             // all entities need to be registered
             configuration.addAnnotatedClass(Customer.class);
             configuration.addAnnotatedClass(Order.class);
             configuration.addAnnotatedClass(Product.class);
             configuration.addAnnotatedClass(SalesManager.class);
+
             ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
                     .applySettings(configuration.getProperties()).build();
             sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
+
         return sessionFactory;
     }
 }
