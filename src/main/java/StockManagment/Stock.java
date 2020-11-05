ebd321426dd4529cf633e275e602dcd1129d4062
pedroiley <pedro.iglesias.leyva@gmail.com>
2020-11-05 21:11:44 +0200

    Add CreateProduct Method

---
+++ src/main/java/StockManagment/Stock.java
@@ -0,0 +1,30 @@
+package StockManagment;
+
+import Enums.Type;
+import Models.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stock {
+
+    public static List<Product> createBasicStock() {
+
+        List<Product> products = new ArrayList<>();
+
+        products.add(new Product(Type.PHONE, 300, "Iphone8", 50));
+        products.add(new Product(Type.PHONE, 500, "Iphone9", 50));
+        products.add(new Product(Type.TABLET, 700, "iPad300", 35));
+        products.add(new Product(Type.TABLET, 500, "Samsung", 40));
+        products.add(new Product(Type.TV, 800, "LG300", 50));
+        products.add(new Product(Type.TV, 1300, "Sony690", 50));
+        products.add(new Product(Type.TOASTER, 200, "Quemaditas50S", 50));
+        products.add(new Product(Type.TOASTER, 100, "Tostadita2000", 50));
+        products.add(new Product(Type.SCOOTER, 600, "Sprint", 20));
+        products.add(new Product(Type.SCOOTER, 900, "FlightEcoS", 50));
+
+        return products;
+    }
+
+
+}
