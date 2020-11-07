374f1856ed1705d137a1ca3ad264bf3a31a5609b
Miquel Vidal <miquelvidalbieto@gmail.com>
2020-11-07 09:23:37 +0200

    Add user validator.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import Dao.ProductDao;
 import Models.Product;
+
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,7 +14,7 @@ public class Main {
         ProductDao productManager = new ProductDao();
         //Scan is what we are going to use to ask the client what they want to do
         Scanner scan = new Scanner(System.in);
-        String userInput = new String();
+        String userInput = null;
 
         //We ask the client what to do
         System.out.println("Please, make a choice for our DATABASE, select GET, START, DELETE or FINISH");
@@ -23,49 +24,46 @@ public class Main {
 
 
         /* Requires a while statement to keep asking the client what option does the client want to do*/
-
-        /*The client wants to get ALL the products in the database*/
-        if (userInput.equals("GET"))
-        {
-            List<Product> listOfProducts = productManager.getProducts();
-            if (listOfProducts.isEmpty())
-            {
-                System.out.println("There are no items in the database");
+        while (true) {
+            while((userInput==null)||(!(userInput.equals("GET")||userInput.equals("START")||(userInput.equals("DELETE")||(userInput.equals("FINISH")))))) {
+                System.out.println("Please, make a choice for our DATABASE, select GET, START, DELETE or FINISH");
+                userInput = scan.next();
             }
-            else
-            {
-                for (Product product: listOfProducts) {
-                    System.out.println(product.getName() + product.getAmount());
+            /*The client wants to get ALL the products in the database*/
+            if (userInput.equals("GET")) {
+                List<Product> listOfProducts = productManager.getProducts();
+                if (listOfProducts.isEmpty()) {
+                    System.out.println("There are no items in the database");
+                } else {
+                    for (Product product : listOfProducts) {
+                        System.out.println(product.getName() + product.getAmount());
+                    }
                 }
+                userInput = null;
             }
-        }
-
-        /* The client wants to add the basic products to the database */
-        else if(userInput.equals("START"))
-        {
-            createBasicStock(productManager);
-            System.out.println("Objects have been added to the database");
-        }
-
-        /* The client wants to remove all the items from the database*/
-        else if(userInput.equals("DELETE"))
-        {
-            List<Product> currentListOfProducts = productManager.getProducts();
-            if (currentListOfProducts.isEmpty())
-            {
-                System.out.println("There are no items to delete");
+            /* The client wants to add the basic products to the database */
+            else if (userInput.equals("START")) {
+                createBasicStock(productManager);
+                System.out.println("Objects have been added to the database");
+                userInput = null;
             }
-            else
-            {
-                cleanStock(productManager);
-                System.out.println("Objects have been removed from the database");
-            }
-        }
 
-        /* The client wants to finish the operations with the database*/
-        else if(userInput.equals("FINISH"))
-        {
-            System.out.println("Thank you for using our database, bye bye");
+            /* The client wants to remove all the items from the database*/
+            else if (userInput.equals("DELETE")) {
+                List<Product> currentListOfProducts = productManager.getProducts();
+                if (currentListOfProducts.isEmpty()) {
+                    System.out.println("There are no items to delete");
+                } else {
+                    cleanStock(productManager);
+                    System.out.println("Objects have been removed from the database");
+                }
+                userInput = null;
+            }
+            /* The client wants to finish the operations with the database*/
+            else if (userInput.equals("FINISH")) {
+                System.out.println("Thank you for using our database, bye bye");
+                break;
+            }
         }
     }
 }
