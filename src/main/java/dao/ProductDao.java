4404da095bea7d8cae7193ed57942ae3da7afb48
Miquel Vidal <miquelvidalbieto@gmail.com>
2020-11-15 13:09:04 +0200

    Add user validation to integers.

--- src/main/java/dao/ProductDao.java
+++ src/main/java/dao/ProductDao.java
@@ -5,6 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import util.Hibernate;
 public class ProductDao {
+
     public ProductDao() {
     }
     public void createProduct(Product product) {
